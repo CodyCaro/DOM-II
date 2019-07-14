9aae2e7581ac65e97e967b36627d8466bd0cea86
CodyCaro <codyccaro@gmail.com>
2019-07-14 14:18:13 -0700

    Created a moveRight and move methods got the move to trigger and the amountToMove to increase

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -3,10 +3,16 @@ const blockHolder = document.querySelector(".blocks");
 class Block {
   constructor(blockElement) {
     this.blockElementProperty = blockElement;
+    this.amountMoved = 0;
 
     this.blockElementProperty.addEventListener(
       "click",
       this.moveToTop.bind(this)
+    );
+
+    this.blockElementProperty.addEventListener(
+      "mousedown",
+      this.moveRight.bind(this)
     );
   }
 
@@ -17,6 +23,19 @@ class Block {
 
   reorderBlocks() {
     blocks = document.querySelectorAll(".block");
+  }
+
+  moveRight() {
+    console.log("holding");
+
+    window.setInterval(this.move.bind(this), 3000);
+  }
+
+  move() {
+    console.log("moving");
+    this.amountMoved += 5;
+    console.log(this.amountMoved);
+    this.blockElementProperty.style.right = `${this.amountMoved}px`;
   }
 }
 
